ded231aa9f9bd352704afb0885a0a6e544cd127f
Lucas Calvetti <lic.calvo099@gmail.com>
2022-10-30 18:47:44 -0300

    changed name of import in router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../pages/Home/index";
+import { Home } from "../pages/home/index";
 import { SearchPetsAround } from "../pages/search-pets-around/index";
-import { Profile } from "../pages/Profile/index";
+import { Profile } from "../pages/profile/index";
 import { LoginValidation } from "../pages/login-validation/index";
 import { UserReportedPets } from "../pages/user-pets-reported/index";
-import { Layout } from "../components/Layout/index";
+import { Layout } from "../components/layout/index";
 import { Registration } from "../pages/registration/index";
 import { ReportNewPet } from "../pages/report-new-pet/index";
 import { EditPet } from "../pages/edit-pet/index";
