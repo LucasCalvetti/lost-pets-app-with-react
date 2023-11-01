e62ab1ab1577420beb0892537f4842e0ccdad94c
Lucas Calvetti <lic.calvo099@gmail.com>
2023-10-31 23:24:24 -0300

    updated API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://lost-pets-app-9zfk.vercel.app";
+const API_URL = "https://pet-finder-8bul.onrender.com";
 
 //---------------------------------------------------------------------------------------//
 
